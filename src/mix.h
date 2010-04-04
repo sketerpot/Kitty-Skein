63e1225beacf9e8fb3a11f341936f867340abf99
user suppressed <pjscott@linux-4.ece.iastate.edu>
2010-04-03 19:51:19 -0500

    Initial commit

---
+++ src/mix.h
@@ -0,0 +1,20 @@
+#ifndef __MIX_H
+#define __MIX_X
+
+#include <stdint.h>
+
+uint64_t rotate_left(uint64_t a, unsigned int shift);
+
+/* Take A and rotate it SHIFT bits to the left. Both A and SHIFT
+   should be simple variables or constants, for safety. */
+#define ROTATE_LEFT(A, SHIFT) (A << SHIFT | A >> (sizeof(A) * 8 - SHIFT))
+
+/* The MIX_{d,j} function takes inputs (x0, x1) and gives outputs (y0,
+   y1). It depends on the rotation constant given, which should be
+   specified as an argument to the macro. */
+#define MIX(ROTCONST, X0, X1, Y0, Y1) do { \
+  Y0 = X0 + X1; \
+  Y1 = ROTATE_LEFT(X1, ROTCONST) ^ Y0; \
+  } while (0)
+
+#endif
---
+++ src/rotation_constants.h
@@ -0,0 +1,124 @@
+/* Threefish rotation constants, expressed as macros so as to make
+   inlining them as easy as possible. */
+#ifndef __ROTATION_CONSTANTS_H
+#define __ROTATION_CONSTANTS_H
+
+/* Threefish-256 constants */
+#define RC256_00 14
+#define RC256_01 16
+#define RC256_10 52
+#define RC256_11 57
+#define RC256_20 23
+#define RC256_21 40
+#define RC256_30 5
+#define RC256_31 37
+#define RC256_40 25
+#define RC256_41 33
+#define RC256_50 46
+#define RC256_51 12
+#define RC256_60 58
+#define RC256_61 22
+#define RC256_70 32
+#define RC256_71 32
+
+/* Threefish-512 constants */
+#define RC512_00 46
+#define RC512_01 36
+#define RC512_02 19
+#define RC512_03 37
+#define RC512_10 33
+#define RC512_11 27
+#define RC512_12 14
+#define RC512_13 42
+#define RC512_20 17
+#define RC512_21 49
+#define RC512_22 36
+#define RC512_23 39
+#define RC512_30 44
+#define RC512_31 9
+#define RC512_32 54
+#define RC512_33 56
+#define RC512_40 39
+#define RC512_41 30
+#define RC512_42 34
+#define RC512_43 24
+#define RC512_50 13
+#define RC512_51 50
+#define RC512_52 10
+#define RC512_53 17
+#define RC512_60 25
+#define RC512_61 29
+#define RC512_62 39
+#define RC512_63 43
+#define RC512_70 8
+#define RC512_71 35
+#define RC512_72 56
+#define RC512_73 22
+
+/* Threefish-1024 constants */
+#define RC1024_00 24
+#define RC1024_01 13
+#define RC1024_02 8
+#define RC1024_03 47
+#define RC1024_04 8
+#define RC1024_05 17
+#define RC1024_06 22
+#define RC1024_07 37
+#define RC1024_10 38
+#define RC1024_11 19
+#define RC1024_12 10
+#define RC1024_13 55
+#define RC1024_14 49
+#define RC1024_15 18
+#define RC1024_16 23
+#define RC1024_17 52
+#define RC1024_20 33
+#define RC1024_21 4
+#define RC1024_22 51
+#define RC1024_23 13
+#define RC1024_24 34
+#define RC1024_25 41
+#define RC1024_26 59
+#define RC1024_27 17
+#define RC1024_30 5
+#define RC1024_31 20
+#define RC1024_32 48
+#define RC1024_33 41
+#define RC1024_34 47
+#define RC1024_35 28
+#define RC1024_36 16
+#define RC1024_37 25
+#define RC1024_40 41
+#define RC1024_41 9
+#define RC1024_42 37
+#define RC1024_43 31
+#define RC1024_44 12
+#define RC1024_45 47
+#define RC1024_46 44
+#define RC1024_47 30
+#define RC1024_50 16
+#define RC1024_51 34
+#define RC1024_52 56
+#define RC1024_53 51
+#define RC1024_54 4
+#define RC1024_55 53
+#define RC1024_56 42
+#define RC1024_57 41
+#define RC1024_60 31
+#define RC1024_61 44
+#define RC1024_62 47
+#define RC1024_63 46
+#define RC1024_64 19
+#define RC1024_65 42
+#define RC1024_66 44
+#define RC1024_67 25
+#define RC1024_70 9
+#define RC1024_71 48
+#define RC1024_72 35
+#define RC1024_73 52
+#define RC1024_74 23
+#define RC1024_75 31
+#define RC1024_76 37
+#define RC1024_77 20
+
+#endif
